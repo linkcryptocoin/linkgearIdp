28a5482dbf9ff693896edc5191092bde51c84dc8
Simon Li <shaw1236@gmail.com>
2018-06-15 05:35:40 +0000

    changed calcGegeBal.js

--- calcGegeBal.js
+++ calcGegeBal.js
@@ -3,10 +3,27 @@
 // Author :  Simon Li
 // Date   : June 16, 2018
 // Linkgear Foundation, All rights reserved
+'use strict';
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 const linkgearPOS = require('./linkgearPOS.js');
 const gegeweb3 = linkgearPOS.gegeweb3();
+const todayStr = new Date().toISOString().substr(0, 10);
+const fileName = '.balanceSheet_<Today>.csv'.replace(/<TODAY>/i, todayStr);
+
+const fs = require('fs')
+console.log(`the file is: ${fileName}`);
+
+try {
+  fs.unlinkSync(fileName);
+  console.log(`successfully deleted ${fileName}`);
+} catch (err) {
+  // handle the error
+}
+
+const logger = fs.createWriteStream(fileName, {
+  flags: 'a' // 'a' means appending (old data will be preserved)
+})
    
 MongoClient.connect(url, function(err, db) {
    if (err) throw err;
@@ -15,23 +32,13 @@ MongoClient.connect(url, function(err, db) {
    const query = {"local.account": {$ne: ""}};
    dbo.collection("users").find(query, {"local.account":1}).toArray(function(err, users) {
        if (err) throw err;
-       
+  
        users.forEach(function(user) {
            const account = user.local.account;
 	   if (account !== "undefined" && gegeweb3.isAddress(account)) { 
               const bal = parseInt(linkgearPOS.balanceOf(account));
-              console.log(`${account}, ${bal}`);
-              /*
-              dbo.collection("BalanceSheet").findAndModify({
-                query: { account: account }, 
-                update: { token: bal },
-                upsert: true,
-                new: true
-             }, function(err, res) {
-                 if (err) throw err;
-                 console.log(`${JSON.stringify(res)}`);
-             });
-             */ 
+              //console.log(`${account}, ${bal}`);
+              logger.write(`${account},${bal}\n`);
            }
        });      
        db.close();
