e265734cc85d3339c18fbd2e32277f443821d9a4
Simon Li <shaw1236@gmail.com>
2018-11-24 20:00:37 +0000

    refactoring the posts report

--- reportPosts.js
+++ reportPosts.js
@@ -1,4 +1,4 @@
-/* Purpose: report Post data
+/* Purpose: report the posting data
      a report from the chainpost database on posts. 
      The fields are author, datetime, number of likes, feedbacks. 
      We need all posts.
@@ -18,28 +18,31 @@ MongoClient.connect(url, function(err, db) {
    if (!dbo)
       console('failed to create a db instance');
 
-   const filter = {"Narrative": 0};
+   const filter = {"Title":1, "postedBy":1, "postedTime":1, "comments":1, "votes":1};
 
    dbo.collection("posts").find({}, filter).toArray(function(err, posts) {
        if (err) throw err;
  
        if (posts.length < 1) throw "No Data found"; 
             
+       console.log(`** Total posts: ${posts.length} **`);
        console.log('***************************************************');
        posts.forEach(function(post) {
            console.log(`Title   : ${post.Title}`);
            console.log(`Author  : ${post.postedBy}`);
            console.log(`Datetime: ${new Date(post.postedTime)}`);
-           console.log(`Number of feedbakcs: ${post.comments.length}`);
+           console.log(`Number of feedbacks: ${post.comments.length}`);
+
            var likeCount = 0;
            post.votes.forEach(function(elem) {
               if (elem.vote === "like")
                  likeCount++; 
            });
 
-           console.log(`Number of like     : ${likeCount}`);
+           console.log(`Number of likes    : ${likeCount}`);
            console.log('***************************************************');
        });
+       console.log(`** Total posts: ${posts.length} **`);
 
        db.close();
    });
