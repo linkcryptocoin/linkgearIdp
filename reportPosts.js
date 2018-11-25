0db71df9581517ebe0959fd123095aa367cd735a
Simon Li <shaw1236@gmail.com>
2018-11-25 01:34:10 +0000

    refactoring reportPosts.js

--- reportPosts.js
+++ reportPosts.js
@@ -23,28 +23,64 @@ MongoClient.connect(url, function(err, db) {
    dbo.collection("posts").find({}, filter).toArray(function(err, posts) {
        if (err) throw err;
  
-       if (posts.length < 1) throw "No Data found"; 
-            
-       console.log(`** Total posts: ${posts.length} **`);
-       console.log('***************************************************');
-       posts.forEach(function(post) {
-           console.log(`Title   : ${post.Title}`);
-           console.log(`Author  : ${post.postedBy}`);
-           console.log(`Datetime: ${new Date(post.postedTime)}`);
-           console.log(`Number of feedbacks: ${post.comments.length}`);
-
-           var likeCount = 0;
-           post.votes.forEach(function(elem) {
-              if (elem.vote === "like")
-                 likeCount++; 
-           });
-
-           console.log(`Number of likes    : ${likeCount}`);
-           console.log('***************************************************');
-       });
-       console.log(`** Total posts: ${posts.length} **`);
+       //showPosts(posts);        // show the report in stored order
+       //showPosts(posts, false); // show the report in ascending order
+       showPosts(posts, true);  // show the report in descending order
 
        db.close();
    });
 });
 
+// Show the posts 
+function showPosts(posts, descSorting) {
+   if (posts.length < 1) throw "No Data found"; 
+
+   if (typeof descSorting == "boolean") {  
+      // Sorting the elements per postedTime - ascending order
+      posts.sort(function(a, b) {
+        return (descSorting)? b.postedTime-a.postedTime: a.postedTime-b.postedTime; 
+      });
+   }
+           
+   console.log(`** Total posts: ${posts.length} **`);
+   console.log('***************************************************');
+   posts.forEach(function(post) {
+       console.log(`Title   : ${post.Title}`);
+       console.log(`Author  : ${post.postedBy}`);
+       console.log(`Datetime: ${getLocalTime(post.postedTime)}`);
+       //console.log(`Datetime: ${getUtcTime(post.postedTime)} (UTC time)`);
+       console.log(`Number of feedbacks: ${post.comments.length}`);
+
+       var likeCount = 0;
+       post.votes.forEach(function(elem) {
+          if (elem.vote === "like")
+             likeCount++; 
+       });
+       console.log(`Number of likes    : ${likeCount}`);
+       console.log('***************************************************');
+   });
+   console.log(`** Total posts: ${posts.length} **`);
+}
+
+/* For AWS, the local time is UTC */
+
+// Get the UTC time string for the post
+function getUtcTime(utcTime) {
+   const date = new Date(utcTime);
+   return date.getUTCFullYear() + "-" + 
+          (date.getUTCMonth() + 1) + "-" + 
+          date.getUTCDate() + " " + 
+          date.getUTCHours() + ":" + 
+          date.getUTCMinutes();
+}
+
+// Get the local time string for the post
+function getLocalTime(utcTime) {
+   const date = new Date(utcTime);
+   
+   return date.getFullYear() + "-" + 
+          (date.getMonth() + 1) + "-" + 
+          date.getDate() + " " + 
+          date.getHours() + ":" + 
+          date.getMinutes();
+}
